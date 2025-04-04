e8ec518f912f9d6754661912177a0caa0d968cdf
ivory willows <105597411+undrscre@users.noreply.github.com>
2025-04-05 01:15:50 +0800

    boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 import axios from 'axios';
+import { Config } from './classes';
 
-export default function() {
+const BASE_URL = "https://nekoweb.org/api";
 
+export class NekowebAPI {
+	config: Config;
+	constructor(config: Config) {
+		this.config = config;
+	}
+
+	// NOTE: expects 'route' and 'config'
+	async generic(route: String) {
+		try {
+			// TODO: add configuration support (auth, etc etc)
+			// NOTE: fuck it's been awhile since i last wrote anything in javascripte -iv
+			//       please do not kill me for my bad code
+			const response = await axios.get(BASE_URL + route);
+			const data = await response.data;
+			return data
+		} catch {
+			throw new Error(`Unable to do generic request to ${BASE_URL + route}`);
+		}
+	}
 }
