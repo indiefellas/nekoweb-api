7407a8a996eb92ab3b5aae44530e08073ae28607
Jb Carreon <jbcarreon212@gmail.com>
2025-04-05 13:45:05 +0800

    Revert "feat(js): node does not support libs in ts, so rewritten in js"
    This reverts commit eee9b305aaeddfb9eaeca22e7617a03e14047b85.

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import type { ZodSchema } from 'zod';
+import { siteSchema, type Site } from './objects';
+import { type Config } from './types';
+
+const BASE_URL = "https://nekoweb.org/api";
+
+export default class NekowebAPI {
+	private config: Config;
+	constructor(config: Config) {
+		this.config = config;
+	}
+
+	async generic<T>(route: String, schema: ZodSchema<T>): Promise<T> {
+		try {
+			const response = await fetch(new URL(BASE_URL + route).href, {
+				method: "GET",
+				headers: { 
+					"Authorization": this.config.apiKey ?? "",
+					"User-Agent": `${this.config.appName || "NekowebAPI"}/1.0`
+				}
+			});
+			if (!response.ok) {
+				throw new Error(`Generic request failed with the code ${response.status}`);
+			}
+			let res = await response.json();
+			return schema.parse(res);
+		} catch (error) {
+			throw new Error(`Failed to do generic request to ${BASE_URL + route}: ${error}`);
+		}
+	}
+
+	async getSiteInfo(username: String = ""): Promise<Site> {
+		if (!username) {
+			if (!this.config.apiKey) throw new Error("Failed to retrieve site info, missing api key");
+			return this.generic<Site>("/site/info", siteSchema)
+		} else {
+			return this.generic<Site>(`/site/info/${username}`, siteSchema)
+		}
+	}
+}
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+//NOTE: yay jsdoc
+export type Config = {
+    /** Your Nekoweb API key. */
+    apiKey: string;
+    /** Your Nekoweb account cookie. */
+    cookie?: string | undefined;
+    /** The application name. */
+    appName?: string | undefined;
+}
