5df1c5ee3cf1d044fb499fccdc0e97ef5244d7a4
ivory willows <105597411+undrscre@users.noreply.github.com>
2025-04-05 11:03:21 +0800

    implement /site/info/*

--- src/classes.ts
+++ src/classes.ts
@@ -1,33 +1,25 @@
-export class Site {
-    id: number = 0;
-    username: string = "";
-    title: string = "";
-    updates: number = 0;
-    followers: number = 0;
-    views: number = 0;
-    createdAt: number = 0;
-    updatedAt: number = 0;
-    createdAtDate: Date = new Date(this.createdAt);
-    updatedAtDate: Date = new Date(this.updatedAt);
+export type Site = {
+    id: number;
+    username: string;
+    title: string;
+    updates: number;
+    followers: number;
+    views: number;
+    createdAt: number;
+    updatedAt: number;
 }
 
-export class Folder {
-    name: string = "";
-    dir: boolean = false;
+export type Folder = {
+    name: string;
+    dir: boolean;
 }
 
-export class BigId {
-    id: string = "";
+export type BigId = {
+    id: string;
 }
 
-export class Config {
-    apiKey: string;
-    cookie: string;
-    appName: string;
-
-    constructor(apiKey: string, cookie: string, appName: string = "nekoweb-api") {
-        this.apiKey = apiKey;
-        this.cookie = cookie;
-        this.appName = appName;
-    }
+export type Config = {
+    apiKey: string | undefined;
+    cookie: string | undefined;
+    appName: string | undefined;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
-import { Config } from './classes';
+import type { Config } from './classes';
 
 const BASE_URL = "https://nekoweb.org/api";
 
 export class NekoAPI {
-	config: Config;
+	private config: Config;
 	constructor(config: Config) {
 		this.config = config;
 	}
 
 	async generic(route: String) {
 		try {
-			const response = await fetch(BASE_URL + route, {
+			const response = await fetch(new URL(BASE_URL + route).href, {
 				method: "GET",
 				headers: { "Authorization": this.config.apiKey ?? "" }
 			});
 			if (!response.ok) {
 				throw new Error(`Generic request failed with the code ${response.status}`);
 			}
-			return response.json
+			return response.json()
 		} catch (error) {
 			throw new Error(`Failed to do generic request to ${BASE_URL + route}: ${error}`);
+		}
+	}
+
+	async getSiteInfo(username: String = "") {
+		if (!username) {
+			// get self
+			if (!this.config.apiKey) throw new Error("Failed to retrieve site info, missing api key");
+			return await this.generic("/site/info")
+		} else {
+			return await this.generic(`/site/info/${username}`)
 		}
 	}
 }
